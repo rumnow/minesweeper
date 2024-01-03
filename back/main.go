c04ff10e38a0284e59c9006466748d054307c995
Roman Ushakov <raushakov@domclick.ru>
2024-01-03 16:34:06 +0100

    ok

--- back/main.go
+++ back/main.go
@@ -48,7 +48,7 @@ func newMineField(size int, difficult byte) mineField {
 }
 // Fill field of mines
 func fillMines(count int, arrField *[]byte) {
-	log.Println("Count:", count, "array:", *arrField)
+	//log.Println("Count:", count, "array:", *arrField)
 	if count == 0 {
 		return
 	}
@@ -126,6 +126,7 @@ func handleNewGame(w http.ResponseWriter, r *http.Request) {
 	difficult := r.URL.Query().Get("difficulty")
 	size, _ := strconv.Atoi(r.URL.Query().Get("size"))
 	//TODO Добавить проверку типов полей
+	log.Println(r.URL)
 	log.Println("Get params difficulty:", difficult,"size:", size)
 	var field2 mineField
 	switch {
